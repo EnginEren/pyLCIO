19a1fb75924c6225a074d4dddab947c1fc073cbc
Engin Eren <engin.eren@desy.de>
2021-01-21 13:00:58 +0100

    shifted z-to-50mm

--- examples/lcio2hdf5.py
+++ examples/lcio2hdf5.py
@@ -94,9 +94,9 @@ def fill_numpy(record):
     #new z-shifted bin in Z
     #binZ = np.arange(-70, 83, 5.088333)
     #new z- asymmetric bin
-    #binZ = np.arange(-100, 55, 5.088333)
+    binZ = np.arange(-90, 65, 5.088333)
     #binZ = np.arange(-45, 110, 5.088333)
-    binZ = np.arange(-35, 121, 5.088333)
+    #binZ = np.arange(-35, 121, 5.088333)
 
     ## Unable to escape using python list here. But we can live with that.
     l = []
