e7c99d38e9f4aa3d9e7358076233dc7625833fad
Engin Eren <engin.eren@desy.de>
2021-01-21 16:17:01 +0100

    fine tuning

--- examples/lcio2hdf5.py
+++ examples/lcio2hdf5.py
@@ -102,7 +102,7 @@ def fill_numpy(record):
 
     #binZ with 40 
     #binZ = np.arange(-80, 125, 5.088333)
-    binZ = np.arange(-90, 115, 5.088333)
+    binZ = np.arange(-91, 114, 5.088333)
 
     ## Unable to escape using python list here. But we can live with that.
     l = []
